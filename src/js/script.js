8412cf6dc662134c39284cd82f099c61b42e42a9
Cat <daniliukdaniil@gmail.com>
2023-12-10 03:51:00 +0300

    header adaptation for <768 screens

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@ const header = document.querySelector("header")
 const scrollChange = 1
 
 //Отслеживаем событие "скролл"
-window.addEventListener('scroll', function() { 
+window.addEventListener('scroll', () => { 
   scrollPosition = window.scrollY;
 
   //Если прокрутили больше, чем мы указали в переменной scrollChange, то выполняется функция добавления класса
@@ -15,4 +15,40 @@ window.addEventListener('scroll', function() {
   } else {
     header.classList.remove("border")
   }
+})
+
+// Реализация работы бургера меню на мобильных разрешениях
+
+const menuHamburger = document.querySelector('.menu-hamburger');
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
+
+menuHamburger.addEventListener('click', () => {
+  menuHamburger.classList.toggle('close');
+  header.classList.toggle('white');
+  menu.classList.toggle('white');
+  overlay.classList.toggle('active');
+})
+
+header.querySelector('.logo').addEventListener('click', () => {
+  menuHamburger.classList.remove('close');
+  header.classList.remove('white');
+  menu.classList.remove('white');
+  overlay.classList.remove('active');
+})
+
+header.querySelector('.btn_close').addEventListener('click', () => {
+  menuHamburger.classList.remove('close');
+  header.classList.remove('white');
+  menu.classList.remove('white');
+  overlay.classList.remove('active');
+})
+
+menu.childNodes.forEach(node => {
+  node.addEventListener('click', () => {
+    menuHamburger.classList.remove('close');
+    header.classList.remove('white');
+    menu.classList.remove('white');
+    overlay.classList.remove('active');
+  })
 })
